ec7936fdbbd47c3e2550035161e8635195705354
paul <10930272@dcsh.edu.tp.tw>
2020-12-17 12:05:36 +0800

    紀錄

--- web/models.py
+++ web/models.py
@@ -8,4 +8,4 @@ class Message(models.Model):
     created = models.DateTimeField('留言時間', auto_now_add=True)
 
     def __str__(self):
-        return seff.user + ':' + self.subject
+        return self.user + ':' + self.subject
--- web/views.py
+++ web/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Message
+from django.urls import reverse_lazy
 
 # Create your views here.
 class MessageList(ListView):
@@ -13,6 +14,7 @@ class MessageCreate(CreateView):
     model = Message
     fields = '__all__'
     success_url = reverse_lazy('mag_list')
+
 
 class MessageDelete(DeleteView):
     model = Message
